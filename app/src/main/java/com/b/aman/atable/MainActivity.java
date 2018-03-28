098f579bdd092150a42be8c23c7e3d3caa598626
amaann <amankumarbostan@gmail.com>
2018-03-28 12:31:46 +0530

    message

--- app/src/main/java/com/b/aman/atable/MainActivity.java
+++ app/src/main/java/com/b/aman/atable/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.PopupMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -38,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        homeWelcomeMassege();
+        homeWelcomeMessage();
         setLogo();
 
         Button btPractice = (findViewById(R.id.bt_practice));
@@ -144,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    public void homeWelcomeMassege(){
+    public void homeWelcomeMessage(){
 
         Toast.makeText(this, "Welcome to aTable", Toast.LENGTH_SHORT).show();
 
